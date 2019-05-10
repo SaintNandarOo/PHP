e29c65033d78432fce3fe71daa63a00ffaecefee
Seint <seint.nandar.oo@digitallaboratory.net>
2019-05-10 14:53:09 +0630

    card-21321678 - PHP Chapter 1

---
+++ example1-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP says Hello</title>
+</head>
+<body>
+<b>
+	<?php 
+	//print("Hello World!");
+	echo "Hello World";
+	 ?>
+</b>
+</body>
+</html>
---
+++ example1-6.php
@@ -0,0 +1,4 @@
+<?php 
+	echo "The population of US is about: ";
+	echo number_format(320853904);
+ ?>
